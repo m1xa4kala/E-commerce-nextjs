c06902e41aa6b6ebccac5c3f962000f8449e713c
m1xa4kala <maikl2020011@gmail.com>
2024-06-05 18:32:11 +0300

    format file

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,17 @@
 import { Box, Flex, Heading } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
-import AuthActions from './AuthActions/AuthActions'
 import Search from '../Search/Search'
+import AuthActions from './AuthActions/AuthActions'
 
 const Header: React.FC = () => {
 	return (
 		<Box as='header' padding={5}>
 			<Flex gap={5} alignItems='center' justifyContent='space-between'>
 				<Link href='/'>
-					<Heading alignSelf='start'>E-commerce</Heading>
+					<Heading alignSelf='start' fontWeight='semibold'>
+						E-commerce
+					</Heading>
 				</Link>
 				<Box flexGrow={1}>
 					<Search />
